2aa464e0728731b097587eeadafdd03ff4c0067b
John1987vlc <foro201025@gmail.com>
2026-02-23 15:21:23 +0100

    feat: implement TASK-008

--- src/assets/js/card.js
+++ src/assets/js/card.js
@@ -8,6 +8,56 @@ export class Card {
         this.abilities = options.abilities || [];
     }
 
+    // Getters
+    get name() {
+        return this._name;
+    }
+
+    get cost() {
+        return this._cost;
+    }
+
+    get attack() {
+        return this._attack;
+    }
+
+    get defense() {
+        return this._defense;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get abilities() {
+        return this._abilities;
+    }
+
+    // Setters
+    set name(value) {
+        this._name = value;
+    }
+
+    set cost(value) {
+        this._cost = value;
+    }
+
+    set attack(value) {
+        this._attack = value;
+    }
+
+    set defense(value) {
+        this._defense = value;
+    }
+
+    set type(value) {
+        this._type = value;
+    }
+
+    set abilities(value) {
+        this._abilities = value;
+    }
+
     play() {
         // Trigger any on-play abilities
         this.abilities.forEach(ability => {
